0a13c73ed40d5433d67c24d3febe1c214fd61aa4
Masud Rana <masud.rana@justgo.com>
2026-04-21 12:25:31 +0600

    done readme

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const d_products = [
+  {
+    id: "p1",
+    title: "Test",
+    price: 6,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "p2",
+    title: "Test2",
+    price: 5,
+    description: "This is a second product - amazing!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {d_products.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            id={product.id}
+          />
+        ))}
       </ul>
     </section>
   );
