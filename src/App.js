a63b723724920463f087b085743a1ed37fae49d8
Masud Rana <masud.rana@justgo.com>
2026-04-22 13:08:50 +0600

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -5,6 +6,20 @@ import { useSelector } from "react-redux";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://reduxcart-b316a-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }, [cart]);
 
   return (
     <Layout>
